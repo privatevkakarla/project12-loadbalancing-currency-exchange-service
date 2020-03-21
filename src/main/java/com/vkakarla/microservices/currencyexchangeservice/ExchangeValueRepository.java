8d4c22cff8aa5d1c9bfbd11aa0c86bfb9dc8c2e9
veerr <veerr@DESKTOP-IQU2JD8>
2020-03-21 06:55:49 +0530

    vrk

---
+++ src/main/java/com/vkakarla/microservices/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,8 @@
+package com.vkakarla.microservices.currencyexchangeservice;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long>
+{
+//creating query method
+ExchangeValue findByFromAndTo(String from, String to);
+}
+
